836685e3f9049a26d33593206d407a2f8018f6ca
auliawidaniq <auddyauraflestari@gmail.com>
2021-01-25 05:20:06 +0700

    First Commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello{
+	public static void main(String[] args){
+		System.out.println("Judul");
+		System.out.println("auliawidaniq: Aplikasi Java 2020");
+		System.out.println("--- *** ---");
+		System.out.println("END");
+		System.out.println("User 2");
+	}
+}
